2d54bdde869e1a595ecb914181b2ef5a2512cb50
Demian Koro <demian.koro@gmail.com>
2022-01-09 21:43:56 -0300

    Agregando Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function Navbar() {
+    return <nav className='nav'>
+                <h1>KALANCHOE</h1>
+                <ul className='ulNav'>
+                    <li>
+                        <a href='./404.html'>HOME</a>
+                    </li>
+                    <li>
+                        <a href='./404.html'>CONTACTO</a>
+                    </li>
+                </ul>
+           </nav>;
+
+}
+
+export default Navbar;
